f45bf68c121dc42e23ee306b6b57a54ca8a77407
FIC <FIC@info.local>
2020-12-17 11:57:09 -0200

    aula08 em andamento

--- Aula08/filmes/src/pages/filmes/index.jsx
+++ Aula08/filmes/src/pages/filmes/index.jsx
@@ -4,15 +4,97 @@ import Jumbotron from '../../components/jumbotron';
 
 class Filmes extends Component {
 
+    // utilizamos o método construtor para setar o que iremos fazer
+    constructor(){
+        // sintaxe do react para uzar o método construtor
+        super();
+
+        // O estado são todas as variaveis que podem sofrer mudanças nos components
+        this.state = {
+            url : "https://5f7f7f14d6aabe00166f064d.mockapi.io/api/filmes",
+            filmes : [],
+            id : '',
+            nome : '',
+            categoria : '',
+            anoLancamento : ''
+        }
+
+    }
+
+    componentDidMount(){
+        this.listar();
+    }
+
+    listar() {
+        fetch(this.state.url,{
+            method : 'GET'
+        })
+        .then(response => response.json())
+        .then(dados => {
+            this.setState({filmes : dados});
+            console.log('filmes state:' + this.state.filmes);
+        })
+        // mostra o erro no console
+        .catch(err => console.error(err));
+    }
+
+    remover(event) {
+        event.preventDefalut();
+
+        console.log(event.target.value);
+
+        fetch(this.state.url,{
+            method : 'DELETE'
+        })
+        .then(response => response.json())
+        .then(dados => {
+            alert('Filme remolvido');
+            this.listar();
+        })
+        .catch(err => console.error(err));
+
+    }
+
+    editar(event) {
+        // impede de recarregar a página
+        event.preventDefalut();
+
+        // mostra no console o valor do event que é pego no input
+        console.log(event.target.value);
+
+        // chamada da API
+        fetch(this.state.url + '/' + event.target.value)
+
+        // then = promise do fetch, onde tratamos as o que está vindo da "API", é como se fosse o que o Postman faz   
+        .then(response => response.json())
+        .then(dado => {
+           this.setState({id : dado.id});
+           this.setState({nome : dado.nome});
+           this.setState({categoria : dado.categoria});
+           this.setState({anoLancamento : dado.anoLancamento});
+        })
+     
+        .catch(err => console.error(err));
+
+    } 
+
+    salvar() {
+        alert(ok);
+    }
+
+   
+// renderiza o menu e o formulário
     render() {
+
         return(
             <div>
+
             <Menu/>
             <Jumbotron titulo='Filmes' descricao='Cadastre seus Filmes'/>
 
                 <div className='container'>
                     <div className='bd-example'>
-                    <form id="formFilme" onSubmit={this.salvar.bind(this)}>
+                    <form id="formFilme" onSubmit={this.Salvar()}>
                             <div className="form-group">
                                 <label htmlFor="nome">Nome</label>
                                 <input type="text" className="form-control" value={this.state.nome} onChange={e => { this.setState({nome : e.target.value})}} id="nome" aria-describedby="nome" placeholder="Informe o Nome"/>
@@ -42,7 +124,24 @@ class Filmes extends Component {
                             <th scope="col"><button type="reset" class="btn"></button></th>
                         </tr>
                     </thead>
-                    <tbody id="tabela-lista-corpo"></tbody>
+                    <tbody id="tabela-lista-corpo">
+                        {
+                            this.state.filmes.map(item => {
+                                return(
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.nome}</td>
+                                    <td>{item.categoria}</td>
+                                    <td>{item.anoLancamento}</td>
+                                    <td>
+                                        <button type="button" className="btn btn-danger" value={item.id} onClick={this.remover.bind(this)}>Remover</button>
+                                        <button type="button" className="btn btn-warning" value={item.id} onClick={this.editar.bind(this)}>Editar</button>
+                                    </td>
+                                </tr>
+                                )
+                            })    
+                        }
+                    </tbody>
                     </table>
                     </div>
                 </div>
@@ -51,6 +150,7 @@ class Filmes extends Component {
             
         )
     }
+
 }
 
 
